a3552eb4a2384e2e22772416c905aaa9120e580a
anonymousboy67 <aashishad67@gmail.com>
2024-03-09 14:34:46 +0545

    FlashCard component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import FlashCards from "./FlashCard/FlashCards.jsx";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FlashCards />
+    </div>
+  );
+}
+
